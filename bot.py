3955d67da74324154a1d5f8c895188e19c32558a
wtt404 <137714878+wtt404@users.noreply.github.com>
2026-05-03 21:03:44 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -220,8 +220,8 @@ async def on_message(message):
     await bot.process_commands(message)
 
     if message.guild:
-    if not translation_enabled.get(message.guild.id, True):
-        return
+        if not translation_enabled.get(message.guild.id, True):
+            return
 
     urls = re.findall(r"https?://[^\s]+", message.content)
 
